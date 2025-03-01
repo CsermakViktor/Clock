f0eefc1205b54bb46350fac01d10e5c6b5f41df6
vcser <Vcsermak41@gmail.com>
2025-03-01 22:56:31 +0100

    Add project files.

---
+++ Border.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Border
+    {
+        public static void clockboarder()
+        {
+            int windowwidth = Console.WindowWidth-1;
+            int windowheight = Console.WindowHeight-1;
+            Console.SetCursorPosition(0, 0);
+            Console.Write("╔");
+            Console.SetCursorPosition(windowwidth, 0);
+            Console.Write("╗");
+            Console.SetCursorPosition(0, windowheight);
+            Console.Write("╚");
+            Console.SetCursorPosition(windowwidth, windowheight);
+            Console.Write("╝");
+            for (int i = 1; i < windowwidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("═");
+                Console.SetCursorPosition(i, windowheight);
+                Console.Write("═");
+            }
+            for (int i = 1; i < windowheight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("║");
+                Console.SetCursorPosition(windowwidth, i);
+                Console.Write("║");
+            }
+        }
+        int borderX;
+        int borderY;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Clock.Border.clockboarder();
+            while (true)
+            {
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                int windowwidth = Console.WindowWidth;
+                int windowheight = Console.WindowHeight;
+
+                Console.SetCursorPosition(windowwidth/2-3, windowheight/2-1);    
+                Console.WriteLine($"{hour:D2}:{minute:D2}:{second:D2}");
+                await Task.Delay(1000);
+                Console.CursorVisible = false;
+
+            }
+        }
+    }
+}
